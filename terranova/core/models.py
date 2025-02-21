26fda3fe75d12abf853b0e383761f2c3a757e162
Mouthieu <mouthieuuu@gmail.com>
2025-02-21 23:52:13 +0100

    Création du CollectionPoint

--- terranova/core/models.py
+++ terranova/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class CollectionPoint(models.Model):
+    name = models.CharField(max_length=255)
+    adress = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return {
+            'name': self.name,
+            'adress': self.adress,
+            'latitude': self.latitude,
+            'longitude': self.longitude
+        }
